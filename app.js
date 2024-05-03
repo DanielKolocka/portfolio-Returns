a0f47bef793ac40e458d059722bd5bcb80dfed82
Daniel <dkolocka@sfu.ca>
2024-05-02 22:25:24 -0700

    Set up app.js and database.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//Initial commit
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.congif({path: './config/config.env'});
+
+app.use(express.json());
+
+
+
+const PORT = process.env.PORT;
+const server = app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}, in ${process.env.NODE_ENV} mode.`);
+});
